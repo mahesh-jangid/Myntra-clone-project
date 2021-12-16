97eb029c86fa6185a3a15515242b316cb0ff4ba5
mahesh-jangid <mesh.jangid@gmail.com>
2021-12-16 19:01:53 +0530

    Product Page Completed

--- main.js
+++ main.js
@@ -1,3 +1,102 @@
+var productData = [
+  {
+    image:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/9793513/2019/6/15/79e8a0aa-18f1-4752-98db-82968506e0491560580186960-Roadster-Men-White-Solid-Round-Neck-T-shirt-3471560580186273-1.jpg",
+    brand_name: "Bushirt",
+    brand_info: "Pure Cotton T-shirt",
+    price: 399,
+    strick_price: 499,
+  },
+  {
+    image:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/12377258/2020/9/11/ce1b7bcb-a65a-4eb0-a317-42ac02718f1e1599798741705UrbanoFashionPrintedMenRoundNeckDarkGreenT-Shirt1.jpg",
+    brand_name: "Urbano Fashion",
+    brand_info: "Slim Tropical T-shirt",
+    price: 494,
+    strick_price: 1099,
+  },
+  {
+    image:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11067734/2019/12/10/34b589c2-80b9-4ad9-81ea-84333fbc46761575972548055-DILLINGER-Men-Tshirts-4071575972546110-1.jpg",
+    brand_name: "Dillinger",
+    brand_info: "Striped Cotton T_shirt",
+    price: 575,
+    strick_price: 1199,
+  },
+  {
+    image:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2378362/2018/6/9/270e0a7e-365b-4640-9433-b269c60bf3061528527188563-Moda-Rapido-Men-Maroon-Printed-Round-Neck-T-shirt-3811528527-1.jpg",
+    brand_name: "H&M",
+    brand_info: "Cotton T-shirt Regular Fit",
+    price: 399,
+    strick_price: 1200,
+  },
+  {
+    image:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/1996777/2017/11/7/11510046815508-Roadster-Men-Black-Solid-Round-Neck-T-shirt-4011510046815294-1.jpg",
+    brand_name: "Roadster",
+    brand_info: "Men Cotton T-shirt",
+    price: 259,
+    strick_price: 399,
+  },
+  {
+    image:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15962726/2021/10/28/d25371a4-5938-440b-9cad-a1ce792f87621635429621395BushirtMenNavyBluePocketsT-shirt1.jpg",
+    brand_name: "Campus Sutra",
+    brand_info: "Colorblocked Cotton T-shirt",
+    price: 408,
+    strick_price: 949,
+  },
+  {
+    image:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2378362/2018/6/9/270e0a7e-365b-4640-9433-b269c60bf3061528527188563-Moda-Rapido-Men-Maroon-Printed-Round-Neck-T-shirt-3811528527-1.jpg",
+    brand_name: "Huetrap",
+    brand_info: "Typography print T-shirt",
+    price: 384,
+    strick_price: 1099,
+  },
+  {
+    image:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15297206/2021/9/27/9637148e-d7d2-477e-86d0-2f930879387f1632727296939-Nautica-Men-Tshirts-5351632727296388-1.jpg",
+    brand_name: "Louis Philippe Jeans",
+    brand_info: "Men T-shirt",
+    price: 714,
+    strick_price: 1299,
+  },
+  {
+    image:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/10940528/2020/2/13/3e484090-d487-4252-9f30-beab10ce1f0a1581594832846-Roadster-Men-Tshirts-4501581594830605-1.jpg",
+    brand_name: "Bushirt",
+    brand_info: "Men Pocket T-shirt",
+    price: 435,
+    strick_price: 1288,
+  },
+  {
+    image:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2020/2/6/1f5471ec-c783-4fa7-99f4-2b6f6fb251761580946317672-1.jpg",
+    brand_name: "Louis Philippe Sport",
+    brand_info: "Solid Polo Collar T-shirt",
+    price: 729,
+    strick_price: 1199,
+  },
+  {
+    image:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/7546902/2019/2/11/70409171-c53c-46ae-9902-e97a03d92ac61549886561252-HERENOW-Men-Teal-Solid-Polo-Collar-T-shirt-7371549886559966-3.jpg",
+    brand_name: "HERE&NOW",
+    brand_info: "Polo Collar Cotton T-shirt",
+    price: 469,
+    strick_price: 1149,
+  },
+  {
+    image:
+      "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/7546902/2019/2/11/70409171-c53c-46ae-9902-e97a03d92ac61549886561252-HERENOW-Men-Teal-Solid-Polo-Collar-T-shirt-7371549886559966-3.jpg",
+    brand_name: "Roadster",
+    brand_info: "Graphic Cotton T-shirt",
+    price: 274,
+    strick_price: 449,
+  },
+];
+
 // -----------------------Navbar animation----------------------------//
 
 function fixedNav() {
@@ -32,3 +131,85 @@ profile_icon.addEventListener("click", function () {
   }
 });
 // -------------End of Profile Card---------------////
+
+// ---------------------For Adding Wishlist Products -----------------------------///
+
+var display = JSON.parse(localStorage.getItem("AllproductItem")) || [];
+
+var wishListArray = JSON.parse(localStorage.getItem("WishListItems")) || [];
+
+// ---------------For Showing Total Wishlist Items---------------------///////////
+
+var getTotalWishListData = JSON.parse(
+  localStorage.getItem("totalWishListItems")
+);
+totalWishListitems();
+function totalWishListitems() {
+  var DOMWishlist = document.querySelector("#wishlist_items");
+
+  var TotalWishListItem = wishListArray.length;
+  DOMWishlist.textContent = TotalWishListItem;
+  localStorage.setItem("totalWishListItems", JSON.stringify(TotalWishListItem));
+}
+
+// ---------------End of  Showing Total Wishlist Items---------------------////
+
+DisplayData(productData);
+
+// -------------------------For Displaying All Products-------------------///
+function DisplayData(productData) {
+  productData.map(function (elem) {
+    var prod_container = document.querySelector(".product_info");
+
+    var productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+
+    var htmlData = `<img
+              src="${elem.image}"
+              alt=""
+            />
+            <div class="content">
+              <h4>${elem.brand_name}</h4>
+              <p>${elem.brand_info}</p>
+               <h4><span id="origin_price">Rs.${elem.price}</span id="strick_price" ><span>Rs.${elem.strick_price}</span></h4>
+            </div>
+            <div class="overlay_card">
+              <button class="w_btn">
+                <i class="fas fa-heart"></i
+                ><a href="" class="wish_btn_text">WHISHLIST</a>
+              </button>
+            </div>`;
+
+    productDiv.insertAdjacentHTML("afterbegin", htmlData);
+
+    prod_container.appendChild(productDiv);
+
+    var wishlist_btn = productDiv.querySelectorAll("button");
+    var wish_btn_text = productDiv.querySelector(".wish_btn_text");
+    wishlist_btn.forEach(function (btn) {
+      btn.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        console.log(e.target.parentElement);
+        if (e.target.parentElement.classList.contains("w_btn")) {
+          this.style.backgroundColor = "black";
+          this.style.color = "red";
+          this.style.transition = "all .5s ease-in";
+          wish_btn_text.style.color = "white";
+          wish_btn_text.textContent = "WHISHLISTED";
+          wish_btn_text.style.fontWeight = "bold";
+        }
+        addInwishlist(elem);
+        totalWishListitems();
+      });
+    });
+  });
+
+  localStorage.setItem("AllproductItem", JSON.stringify(productData));
+}
+// -------------------------End of  Displaying All Products-------------------///
+
+function addInwishlist(whishProd) {
+  wishListArray.push(whishProd);
+  localStorage.setItem("WishListItems", JSON.stringify(wishListArray));
+}
